eac1fe964f226cbc98a4c2c9808892eea0615279
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2017-06-10 20:04:32 +0200

    Utilise CentraleSupélec pour le ©

--- Application/src/main/java/fr/centralesupelec/students/clientble/GattConstants.java
+++ Application/src/main/java/fr/centralesupelec/students/clientble/GattConstants.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
- * Copyright (C) 2017 Louis-Guillaume Dubois
+ * Copyright (C) 2017 CentraleSupélec
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
